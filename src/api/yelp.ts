4d47794ae151703ec1257938229bff3adaf757ae
Bogdan S <bseleng@gmail.com>
2022-03-24 18:17:50 +0300

    SEC-9 | fix: remove secret API key

--- src/api/yelp.ts
+++ src/api/yelp.ts
@@ -3,6 +3,6 @@ import axios from 'axios'
 export default axios.create({
   baseURL: 'https://api.yelp.com/v3/businesses',
   headers: {
-    Authorization: 'Bearer zkfKzHZpJTfd05g4HoQ0s5g8cbuliln6oGw5lBrhewic7tAsKU0ISI69XSIxn7_iAC94WHd6MVrPz-N-qbTsTwUf-XMLLfBGBx6sgenJZy-31zg7uMNJkL_Y7oYzYnYx'
+    Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}`
   }
 })
